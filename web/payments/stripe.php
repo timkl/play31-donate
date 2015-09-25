4eaff087947c42c9cbb6f763673f2fd7f19c0544
Domenico Matteo <matteo.domenico@gmail.com>
2015-09-25 14:20:30 +0200

    dir revolution \o/

---
+++ web/payments/stripe.php
@@ -0,0 +1,18 @@
+<?php
+  require '../../vendor/stripe/stripe-php/init.php';
+  if ($_POST) {
+    \Stripe\Stripe::setApiKey("sk_test_ezB4z3SpCMH55873RqC4GK2A");
+    $error = '';
+    $success = '';
+    try {
+      if (!isset($_POST['stripeToken']))
+        throw new Exception("The Stripe Token was not generated correctly");
+      \Stripe\Charge::create(array("amount" => 1000,
+                                  "currency" => "usd",
+                                  "card" => $_POST['stripeToken']));
+      header('Location: http://play31.herokuapp.com/thankyou.html');
+    }
+    catch (Exception $e) {
+      $error = $e->getMessage();
+    }
+  }
